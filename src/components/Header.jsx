e499bd2f90fb34ee9eea8855e7df1ec529948b01
Mohd Zain <mohammedzain4099@gmail.com>
2025-03-23 18:21:39 +0530

    Update Header component background image style and modify Vite config

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,8 +5,7 @@ import { motion } from "framer-motion";
 function Header() {
   return (
     <div
-      className="min-h-screen mb-4 bg-cover bg-center flex items-center w-full overflow-hidden"
-      style={{ backgroundImage: "url('/header_img.png')" }}
+      className="min-h-screen mb-4 bg-cover bg-center flex items-center w-full overflow-hidden bg-[url('/header_img.png')]"
       id="Header"
     >
       <Navbar />
